e942ed548387c7495b680a9af1051624878d953f
Laabsky <jlaabs@madisoncollege.edu>
2018-04-10 18:09:28 -0500

    test

--- src/test/java/fitness/service/FitnessServiceTest.java
+++ src/test/java/fitness/service/FitnessServiceTest.java
@@ -64,7 +64,7 @@ public class FitnessServiceTest {
     @Test
     void calculateFatProtienCarboHyderatesRequiredPerDayHtml() {
         Response response = fitnessService.calculateFatProtienCarboHyderatesRequiredPerDayHtml(100, 178, 37, "m", "sed");
-        assertEquals("<h3>Maintain weight</h3><table border = '1'><tbody><tr><td><b>Daily intake</b></td><td><b>grams</b><"
+        assertEquals("<html><head><title>Fat Protein Carbohydrates Result Page</title></head><body><p><h3>Maintain weight<"
                 , response.getEntity().toString().substring(0,100));
     }
 
@@ -96,5 +96,52 @@ public class FitnessServiceTest {
         assertEquals("<h3>Calories burned: 227</h3>"
                 , response.getEntity().toString());
     }
+
+    /**
+     * Verifies BMR's Imperial calculations with JSON output.
+     */
+    @Test
+    void testBMIcalculationsImperial() {
+        Response bmi = fitnessService.calculateBMIlbs("json",200,69);
+        assertEquals("{\"Height\":69,\"Weight\":200,\"BMI\":\"29.53\"}", bmi.getEntity());
+    }
+
+    /**
+     * Verifies BMR's Metric calculations with JSON output.
+     */
+    @Test
+    void testBMIcalculationsMetric() {
+        Response bmi = fitnessService.calculateBMIkg("json",95,1.9);
+        assertEquals("{\"Height\":1.9,\"Weight\":95,\"BMI\":\"26.32\"}", bmi.getEntity());
+    }
+
+    /**
+     * Verifies BMR's Imperial calculations with JSON output.
+     */
+    @Test
+    void testBMIGetCalculationsImperial() {
+        Response bmi = fitnessService.calculateGetBMIlbs("json",200,69);
+        assertEquals("{\"Height\":69,\"Weight\":200,\"BMI\":\"29.53\"}", bmi.getEntity());
+    }
+
+    /**
+     * Verifies BMR's Metric calculations with JSON output.
+     */
+    @Test
+    void testBMIGetCalculationsMetric() {
+        Response bmi = fitnessService.calculateGetBMIkg("json",95,1.9);
+        assertEquals("{\"Height\":1.9,\"Weight\":95,\"BMI\":\"26.32\"}", bmi.getEntity());
+    }
+
+    /**
+     * Verifies BMR's Metric calculations with JSON output.
+     */
+    @Test
+    void testBMIGetCalculationsMetricHtml() {
+        Response bmi = fitnessService.calculateGetBMIkg("html",95,1.9);
+        assertEquals("<h3>BMI</h3><p>The BMI for a person with a height of 1.9 and a weight of 95.0 is : 26.32</p>", bmi.getEntity());
+    }
+
+
 
 }
